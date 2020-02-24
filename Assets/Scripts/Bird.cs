38eece10da6d9b3f8e7e1cab12521c806f4e0dc3
RECNEPS-KIPS <2432719656@qq.com>
2020-02-24 14:53:04 +0800

    实现了对象位置坐标的限定

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour {
     private bool isClick;
+    public Vector3 launchPos;
+    public float maxDis;
     // Start is called before the first frame update
     void Start() {
         isClick = false;
+        launchPos=GameObject.Find("LaunchPos").transform.position;
     }
 
     // Update is called once per frame
@@ -18,8 +21,15 @@ public class Bird : MonoBehaviour {
             //transform.position = new Vector3(transform.position.x,transform.position.y,0);
             //2.坐标减去相机的z轴坐标
             transform.position -= new Vector3(0,0, Camera.main.transform.position.z);
-
-
+            
+            //位置限定
+            //若对象的位置距离发射位置大于maxDis
+            if (Vector3.Distance(transform.position,launchPos)>maxDis) {
+                //发射位置指向对象位置的方向向量(归一化)
+                Vector3 dir =(transform.position - launchPos).normalized;
+                dir *= maxDis;//方向乘距离得到长度向量
+                transform.position = dir + launchPos;//发射位置+长度向量得到对象被限定的位置坐标
+            }
         }
     }
     //鼠标按下
