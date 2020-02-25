db8ac6df51ef19da89885a1ceee1398ea986308c
RECNEPS-KIPS <2432719656@qq.com>
2020-02-25 16:32:20 +0800

    实现了绿皮猪的死亡和受伤
    逻辑原理:collision.relativeVelocity表示相对速度(向量),magnitude表示该向量的模长,相对速度的模长大于一定数值,死亡逻辑成立

--- Assets/Scripts/Pig.cs
+++ Assets/Scripts/Pig.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pig : MonoBehaviour
-{
+public class Pig : MonoBehaviour {
+    //鸟的速度大于10,则绿皮猪死亡,大于5小于10则受伤
+    public float maxSpeed=10;
+    public float minSpeed = 5;
+
+    public SpriteRenderer sr;
+    public Sprite pigHurtSprite;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    //碰撞检测
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.transform.tag=="Player") {
+            //collision.relativeVelocity表示相对速度(向量),magnitude表示该向量的模长
+            //死亡
+            if (collision.relativeVelocity.magnitude>maxSpeed) {
+                Destroy(this.gameObject);
+            }
+            //受伤
+            else if (collision.relativeVelocity.magnitude >minSpeed&& collision.relativeVelocity.magnitude <maxSpeed) {
+                sr.sprite = pigHurtSprite;
+            }
+        }
     }
 }
