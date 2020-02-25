addefb4ed99371eadf67502632ef78f5768d6d6c
RECNEPS-KIPS <2432719656@qq.com>
2020-02-25 17:59:06 +0800

    制作了Boom动画
    将动画的每一帧贴图选中,集体拖拽到场景中,会自动生成一个动画,Ctrl+6打开这个动画的Animation,可以编辑每一帧的动画

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -21,24 +21,23 @@ public class Bird : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         isClick = false;
-        launchPos=GameObject.Find("LaunchPos").transform.position;
+        launchPos = GameObject.Find("LaunchPos").transform.position;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         if (isClick) {
             transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);//将鼠标坐标转化到屏幕空间坐标系
             //1.将坐标的z轴限定为0
             //transform.position = new Vector3(transform.position.x,transform.position.y,0);
             //2.坐标减去相机的z轴坐标
-            transform.position -= new Vector3(0,0, Camera.main.transform.position.z);
-            
+            transform.position -= new Vector3(0, 0, Camera.main.transform.position.z);
+
             //位置限定
             //若对象的位置距离发射位置大于maxDis
-            if (Vector3.Distance(transform.position,launchPos)>maxDis) {
+            if (Vector3.Distance(transform.position, launchPos) > maxDis) {
                 //发射位置指向对象位置的方向向量(归一化)
-                Vector3 dir =(transform.position - launchPos).normalized;
+                Vector3 dir = (transform.position - launchPos).normalized;
                 dir *= maxDis;//方向乘距离得到长度向量
                 transform.position = dir + launchPos;//发射位置+长度向量得到对象被限定的位置坐标
             }
@@ -58,7 +57,8 @@ public class Bird : MonoBehaviour {
         //不接受物理影响
         r2d.isKinematic = false;
         //延迟调用,等待物理计算完成之后再将springJoint失效
-        Invoke("Fly",0.1f);
+        Invoke("Fly", 0.1f);
+        
     }
 
     void Fly() {
@@ -69,9 +69,12 @@ public class Bird : MonoBehaviour {
 
     //绘制橡皮筋
     void DrawLine() {
-        lrRight.SetPosition(0,rightPos.position);
-        lrRight.SetPosition(1,transform.position);
-        lrLeft.SetPosition(0,leftPos.position);
-        lrLeft.SetPosition(1,transform.position);
+        //lrRight.SetPosition(0,rightPos.position);
+        //lrRight.SetPosition(1,transform.position);
+        //lrLeft.SetPosition(0,leftPos.position);
+        //lrLeft.SetPosition(1,transform.position);
+
+        lrRight.SetPositions(new[] { rightPos.position, transform.position });
+        lrLeft.SetPositions(new[] { leftPos.position, transform.position });
     }
 }
