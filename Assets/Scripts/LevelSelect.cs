0c09b9fe70d2c43400a86ed2e67f3f2e989ffbbb
RECNEPS-KIPS <2432719656@qq.com>
2020-03-01 22:09:03 +0800

    实现了默认关卡的解锁逻辑

---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelect : MonoBehaviour {
+    public bool canSelect = false;
+    public Image image;
+    public Sprite unlockSprite;
+    // Start is called before the first frame update
+    void Start() {
+        image = GetComponent<Image>();
+        //解锁第一个关卡
+        if (transform.parent.GetChild(0).name == gameObject.name) {
+            canSelect = true;
+        }
+
+        if (canSelect) {
+            image.sprite = unlockSprite;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
