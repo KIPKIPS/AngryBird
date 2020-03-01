03a32ee5c3623c49759369343e00846e32594b83
RECNEPS-KIPS <2432719656@qq.com>
2020-03-01 21:34:22 +0800

    实现了地图选择的逻辑

---
+++ Assets/Scripts/MapSelect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSelect : MonoBehaviour {
+    public int starNum;//解锁所需星星数量
+    public bool canSelect;//关卡是否可以被选择
+
+    public GameObject stars;
+    public GameObject locks;
+    // Start is called before the first frame update
+    void Start() {
+        if (PlayerPrefs.GetInt("totalNumOfStar", 0)>=starNum) {
+            canSelect = true;
+        }
+
+        if (canSelect) {
+            locks.SetActive(false);
+            stars.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
