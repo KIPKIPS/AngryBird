002713c0f997c0b14663246d641d485936de0c54
RECNEPS-KIPS <2432719656@qq.com>
2020-03-01 22:16:26 +0800

    完善了关卡显示的逻辑

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -7,16 +7,19 @@ public class LevelSelect : MonoBehaviour {
     public bool canSelect = false;
     public Image image;
     public Sprite unlockSprite;
+
+    public GameObject levelNum;
     // Start is called before the first frame update
     void Start() {
-        image = GetComponent<Image>();
+        image = GetComponent<Image>();//获取Image组件
         //解锁第一个关卡
         if (transform.parent.GetChild(0).name == gameObject.name) {
             canSelect = true;
         }
-
+        //若可以选择关卡,将图片替换成解锁图片
         if (canSelect) {
             image.sprite = unlockSprite;
+            levelNum.SetActive(true);
         }
     }
 
