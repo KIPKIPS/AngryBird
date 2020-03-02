d2fbba74f5888b05f35f126c87c186a48126aac2
RECNEPS-KIPS <2432719656@qq.com>
2020-03-02 20:43:59 +0800

    修复了关卡图片错误的问题

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class LoadLevel : MonoBehaviour {
     void Awake() {
+        //获取当前地图
         string currentMap = PlayerPrefs.GetString("CurrentMap");
+        //加载当前地图当前关卡对应的关卡场景对象
         GameObject currentLevel=Resources.Load<GameObject>("Levels/"+currentMap+"/"+PlayerPrefs.GetString("CurrentLevel"));
         Instantiate(currentLevel, Vector3.zero, Quaternion.identity);
     }
