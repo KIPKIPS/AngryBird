64c998407c4afcb40716a4c944046618fc02ad8e
RECNEPS-KIPS <2432719656@qq.com>
2020-03-03 12:42:30 +0800

    实现了Playerprefs数据的清除

---
+++ Assets/Scripts/Test/DeletePlayerPrefsData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeletePlayerPrefsData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDeleteButtonEvent() {
+        PlayerPrefs.DeleteAll();
+    }
+}
